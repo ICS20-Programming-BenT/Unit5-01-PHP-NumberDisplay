eddb1f04316df6f8b3980b4ea0c673df0a8f347f
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-05-08 23:04:22 +0000

    Fixed errors

--- results.php
+++ results.php
@@ -10,34 +10,33 @@ $maxNumber = $_POST["max-number"];
 
 // If the user does not enter their min and max, display that they must enter both numbers
 if (($minNumber == "") || ($maxNumber == "")) {
-  $message = "Please enter both a minimum and a maximum number.";
+    $message = "Please enter both a minimum and a maximum number.";
 }
 
 // Otherwise, continue the function to list the numbers
 else {
-  // Initializing the counter variable to the minimum number
-  $counter = $minNumber;
+    // Initializing the counter variable to the minimum number
+    $counter = $minNumber;
   
-  // If statement to check whether minNumber > maxNumber (there is an error)
-  if ($minNumber > $maxNumber) {
-      $message = "Please be sure that your minimum value is less than or equal to your maximum value.";
-  }
-  
-  // Else, complete the while loop to create list of the numbers between min and max
-  else {
-    // While loop that specifies that the code within the loop executes as long as the counter is less than or equal to the maximum
-    while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
-      
-      // Building the string of numbers with a line break
-      $numbers = $numbers . $counter . "<br>";
-  
-      // Increment the counter with each number
-      $counter++;
-  
-      // Set the message variable to display results
-      $message = "Here are all the numbers between the minimum and maximum that you entered:<br><br>" . $numbers;
+    // If statement to check whether minNumber > maxNumber (there is an error)
+    if ($minNumber > $maxNumber) {
+        $message = "Please be sure that your minimum value is less than or equal to your maximum value.";
     }
-  }
+  
+    // Else, complete the while loop to create list of the numbers between min and max
+    else {
+        // While loop that specifies that the code within the loop executes as long as the counter is less than or equal to the maximum
+        while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
+          
+            // Building the string of numbers with a line break
+            $numbers = $numbers . $counter . "<br>";
+        
+            // Increment the counter with each number
+            $counter++;
+        }
+        // Set the message variable to display results outside the while loop
+        $message = "Here are all the numbers between the minimum and maximum that you entered:<br><br>" . $numbers;
+    }
 }
 
 // Displaying the results (the range of numbers) back to the user
